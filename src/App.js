77756e3cd427690f627a499f31aa591002e7ec68
sailakshmi <sailakshmibv2004@gmail.com>
2024-05-31 02:09:26 +0000

    passwordManager

--- src/App.js
+++ src/App.js
@@ -181,7 +181,7 @@ class App extends Component {
           <div className="show-passwords">
             <input
               type="checkbox"
-              id="check"
+              id="checked"
               onChange={this.showPassword}
               className="check-box"
             />
@@ -221,7 +221,7 @@ class App extends Component {
                   <button
                     type="button"
                     onClick={() => this.deleteItem(eachValue.id)}
-                    className="del-btn" test-id="delete"
+                    className="del-btn"
                   >
                     <img
                       src="https://assets.ccbp.in/frontend/react-js/password-manager-delete-img.png "
